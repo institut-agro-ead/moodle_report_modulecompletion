901ac02301ca91c0298500712ced4731f4c5d7f8
Sébastien Viallemonteil <ead-hotline@agrosupdijon.fr>
2024-04-05 10:43:18 +0200

    New release

--- version.php
+++ version.php
@@ -24,8 +24,8 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version = 2023082800; // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version = 2024040500; // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires = 2022041200; // Requires this Moodle version.
 $plugin->component = 'report_modulecompletion'; // Full name of the plugin (used for diagnostics).
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = '4.2.0.2';
+$plugin->release = '4.2.0.3';
