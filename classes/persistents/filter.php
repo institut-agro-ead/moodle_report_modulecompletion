4be3ad738d3ce8e779231bc2b9e4ffd6b54048ea
Sébastien Viallemonteil <ead-hotline@agrosupdijon.fr>
2025-04-02 10:27:54 +0200

    Removing unwanted blank lines

--- classes/persistents/filter.php
+++ classes/persistents/filter.php
@@ -14,7 +14,6 @@
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
-
 namespace report_modulecompletion\persistents;
 
 use core\persistent;
